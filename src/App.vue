7be2581e6f4cfab20eeab76fbccd272741f542bb
esposito.00@outlook.it <esposito.00@outlook.it>
2022-12-12 18:37:16 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup>
+<script>
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 import Footer from './components/Footer.vue'
+
+export default {
+
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+
+}
 </script>
 
 <template>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,42 +2,158 @@
 import { ref } from 'vue'
 </script>
 
+export default {
+
+}
+
 <template>
-    <footer class="conteiner">
-        <section class="section1">
-            <ul>
-                <li>
-                    <a href="#">DIGITAL COMICS</a>
-                </li>
-                <li>
-                    <a href="#">DIGITAL COMICS</a>
-                </li>
-                <li>
-                    <a href="#">DIGITAL COMICS</a>
-                </li>
-                <li>
-                    <a href="#">DIGITAL COMICS</a>
-                </li>
-                <li>
-                    <a href="#">DIGITAL COMICS</a>
-                </li>
-            </ul>
-        </section>
-        <section class="section2"></section>
-        <section class="section3"></section>
+    <footer>
+        <div class="container">
+            <section class="section1">
+                <ul>
+                    <li>
+                        <a href="#">DIGITAL COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">DIGITAL COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">DIGITAL COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">DIGITAL COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">DIGITAL COMICS</a>
+                    </li>
+                </ul>
+            </section>
+            <section class="section2">
+                <div>
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                    </ul>
+
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h4>DC</h4>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                </div>
+
+                <div>
+                    <h4>SITES</h4>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                </div>
+            </section>
+            <section class="section3"></section>
+        </div>
     </footer>
 </template>
 
 <style scoped>
-footer {
-    background-color: red;
-    color: yellow;
-    height: 1000px;
+footer li {
+    list-style-type: none;
 }
+
 
 footer .section1 {
     padding: 20px;
-    padding-bottom: 50px;
-    background-color: blue;
+    width: 100%;
+    padding: 50px 0;
+    background-color: rgb(61, 157, 198);
+}
+
+.section1 ul {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+}
+
+footer .section2 {
+    gap: 50px;
+    display: flex;
 }
 </style>
